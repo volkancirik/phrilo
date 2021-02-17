243386b934a5d62ed2d8557b5e0f0ec3f0a8e2d3
Volkan Cirik <vcirik@cs.cmu.edu>
2021-02-17 11:00:01 -0500

    fixing install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env/bash
-
+set -e
 conda create -n phrilo python=3.7 --yes
+source `which python | sed 's/bin\/python/etc\/profile.d\/conda.sh/g'`
 conda activate phrilo
-
 conda install pytorch torchvision torchaudio cudatoolkit=11.0 -c pytorch -y
 conda install h5py ipykernel ipywidgets tqdm scikit-image -y
 conda install -c conda-forge spacy -y
 
-pip install pytorch-pretrained-bert
 python -m ipykernel install --user --name ban3.5 --display-name "PHRILO"
 python -m spacy download en
 python -m pip install --upgrade --user ortools
+pip install pytorch-pretrained-bert
