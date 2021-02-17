ef92bd76f88f9e596ffe7a8be7ec69c8b0fd4c12
Volkan Cirik <vcirik@splicer>
2021-02-17 11:06:56 -0500

    install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ conda install pytorch torchvision torchaudio cudatoolkit=11.0 -c pytorch -y
 conda install h5py ipykernel ipywidgets tqdm scikit-image -y
 conda install -c conda-forge spacy -y
 
-python -m ipykernel install --user --name ban3.5 --display-name "PHRILO"
+python -m ipykernel install --user --name phrilo --display-name "PhrILo"
 python -m spacy download en
 python -m pip install --upgrade --user ortools
 pip install pytorch-pretrained-bert
